c479b826026a3091c5c35d6536b16e3d76d1df35
salmunoz <munoz.sal3@gmail.com>
2016-03-15 18:58:34 -0500

    reducing the amount of current_client id and conditionals in the controllers of foreclosure and rentals.

--- app/controllers/law_projects_controller.rb
+++ app/controllers/law_projects_controller.rb
@@ -9,8 +9,24 @@ class LawProjectsController < ApplicationController
 
 
 	def create
-    if client_signed_in?
-	    @law_project = LawProject.new({violation_type: params[:violation_type], client_id: current_client.id })
+    # if client_signed_in?
+	    @law_project = LawProject.new({
+	    	client_id: params[:id],
+	     violation_type: params[:violation_type], 
+				current_evicted: params[:current_evicted],
+				community_group: params[:community_group],
+				tech_assistance: params[:tech_assistance],
+				frclsr_prevention: params[:frclsr_prevention],
+				low_income_family: params[:low_income_family],
+				displacement: params[:displacement],
+				substandard_housing: params[:substandard_housing],
+				disabilities: params[:disabilities],
+				lucha_tenant: params[:lucha_tenant],
+				law_project_chargable: params[:law_project_chargable],
+				other: params[:other],
+				census_tract: params[:census_tract]
+
+	    	 })
 	    if @law_project.save
 	    flash[:success] = "You've completed the law_project application"
 	    redirect_to "/clients/#{@law_project.client_id}"
@@ -18,15 +34,15 @@ class LawProjectsController < ApplicationController
 	      render :create
 	    end
 
-	  elsif user_signed_in?
-	    @law_project = LawProject.new({violation_type: params[:violation_type], client_id: paramms[:client_id] })
-	    if @law_project.save
-	    flash[:success] = "You've completed the law_project application"
-	    redirect_to "/clients/#{@law_project.client_id}"
-	    else
-	      render :create
-	    end
-	  end
+	  # elsif user_signed_in?
+	  #   @law_project = LawProject.new({violation_type: params[:violation_type], client_id: paramms[:client_id] })
+	  #   if @law_project.save
+	  #   flash[:success] = "You've completed the law_project application"
+	  #   redirect_to "/clients/#{@law_project.client_id}"
+	  #   else
+	  #     render :create
+	  #   end
+	  # end
   end
 	
 
@@ -36,8 +52,22 @@ class LawProjectsController < ApplicationController
 
 	def update
 		@law_project = LawProject.where(client_id: params[:id])
-		if @law_project.update({violation_type: params[:violation_type], 
-        })
+		@law_project.update(
+			violation_type: params[:violation_type], 
+			current_evicted: params[:current_evicted],
+			community_group: params[:community_group],
+			tech_assistance: params[:tech_assistance],
+			frclsr_prevention: params[:frclsr_prevention],
+			low_income_family: params[:low_income_family],
+			displacement: params[:displacement],
+			substandard_housing: params[:substandard_housing],
+			disabilities: params[:disabilities],
+			lucha_tenant: params[:lucha_tenant],
+			law_project_chargable: params[:law_project_chargable],
+			other: params[:other],
+			census_tract: params[:census_tract]
+    )
+		if
 			flash[:success] = "law_project application submitted."
 			redirect_to '/clients/#{@law_project.client_id}'
 		else
