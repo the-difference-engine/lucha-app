deea2a42445bb022984d4ef47d295df7398dab95
salmunoz <munoz.sal3@gmail.com>
2016-03-18 17:09:57 -0500

    created helper file for states

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_controller.include_all_helpers = true
+
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_controller.include_all_helpers = true
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
