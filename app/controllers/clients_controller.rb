4df8b305f15dfec1e1522b27517f6f8291d7dcb3
zoe heins <zcheins@gmail.com>
2016-04-13 20:45:56 -0500

    update action in client contoller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -51,37 +51,35 @@ class ClientsController < ApplicationController
 
 
     # This is how I handled the updating before the Best_in_Place gem
-    # @client.update({
-    # first_name: params[:first_name],
-    # last_name: params[:last_name],
-    # email: params[:email],
-    # password: params[:password],
-    # race: params[:race],
-    # sex: params[:sex], 
-    # home_phone: params[:home_phone], 
-    # work_phone: params[:work_phone], 
-    # cell_phone: params[:cell_phone], 
-    # address: params[:address], 
-    # state: params[:state], 
-    # city: params[:city], 
-    # zip_code: params[:zip_code],
-    # ward: params[:ward],
-    # ssn: params[:ssn],
-    # preferred_contact_method: params[:preferred_contact_method],
-    # preferred_language: params[:preferred_language],
-    # dob: params[:dob],
-    # head_of_household: params[:head_of_household],
-    # num_in_household: params[:num_in_household],
-    # num_of_dependants: params[:num_of_dependants],
-    # education_level: params[:education_level],
-    # disability: params[:disability],
-    # union_member: params[:union_member],
-    # military_service_member: params[:military_service_member],
-    # volunteer_interest: params[:volunteer_interest],
-    # estimated_household_income: params[:estimated_household_income],
-    # authorization_and_waiver: params[:authorization_and_waiver],
-    # privacy_policy_authorization: params[:privacy_policy_authorization]
-    #   })
+    @client.update({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      home_phone: params[:home_phone], 
+      cell_phone: params[:cell_phone],
+      work_phone: params[:work_phone], 
+      address: params[:address], 
+      state: params[:state], 
+      city: params[:city], 
+      zip_code: params[:zip_code],
+      ward: params[:ward], 
+      sex: params[:sex], 
+      race: params[:race],
+      ssn: params[:ssn],
+      preferred_contact_method: params[:preferred_contact_method],
+      preferred_language: params[:preferred_language],
+      marital_status: params[:marital_status],
+      dob: params[:dob],
+      head_of_household: params[:head_of_household] || false,
+      num_in_household: params[:num_in_household],
+      num_of_dependants: params[:num_of_dependants],
+      education_level: params[:education_level],
+      estimated_household_income: params[:estimated_household_income],
+      disability: params[:disability] || false,
+      union_member: params[:union_member] || false,
+      military_service_member: params[:military_service_member] || false,
+      volunteer_interest: params[:volunteer_interest] || false
+
+      })
     
 
 
