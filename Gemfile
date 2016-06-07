02e146323b9e3a3551fb2d9120a5c6c9d0404dc8
Visraant <visraant@gmail.com>
2016-06-06 19:30:55 -0500

    able to export excel and csv foreclosure apps for users

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use mysql as the database for Active Record
-gem 'mysql2', '~> 0.3.10'
 gem 'seed_dump'
 gem 'ruby_outlook'
 gem 'rails-i18n', '~> 4.0.0'
--- app/models/foreclosure.rb
+++ app/models/foreclosure.rb
@@ -12,20 +12,20 @@ class Foreclosure < ActiveRecord::Base
   # validates_format_of :employer_phone, :with => ^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-‌​9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})
 
 
-	has_many :program_employees, as: :programable
-	belongs_to :client
+  has_many :program_employees, as: :programable
+  belongs_to :client
 
 
-	def counselor?
-		if program_employees[0].user.blank?
-			"Not yet assigned."
-		else 
-			program_employees[0].user
-		end
-	end
+  def counselor?
+    if program_employees[0].user.blank?
+     "Not yet assigned."
+   else 
+     program_employees[0].user
+   end
+ end
 
-	def column_count
-		attributes.length - 3
+ def column_count
+  attributes.length - 3
 		# Subtracting 3 from the column count to account for the columns id, updated at, and created at, which were filled automatically and not by the client.
 
 	end
@@ -39,5 +39,13 @@ class Foreclosure < ActiveRecord::Base
     end
     filled_count - 3
   end
-	
+
+def self.to_csv(options = {})
+  CSV.generate(options) do |csv|
+    csv << column_names
+    all.each do |foreclosure|
+      csv << foreclosure.attributes.values_at(*column_names)
+    end
+  end
+end
 end
