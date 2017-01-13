902f619b2254799cf6bc86a67296343f7a5e7076
verynear <scott.a.near@gmail.com>
2017-01-12 18:33:15 -0600

    homebuying rspec test

--- spec/controllers/homebuyings_controller_spec.rb
+++ spec/controllers/homebuyings_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HomebuyingsController, type: :controller do
 
+	describe "index #GET" do
+		it "assigns an array of Homebuying" do
+			client = create(:client)
+			p create(:homebuying, client_id: client.id)
+		end
 end
