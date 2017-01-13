6e57848e54984e8b1b05c70139db6ab3e75591a2
verynear <scott.a.near@gmail.com>
2017-01-12 20:58:02 -0600

    successfully tested homebuying create method"

--- app/controllers/homebuyings_controller.rb
+++ app/controllers/homebuyings_controller.rb
@@ -12,30 +12,26 @@ class HomebuyingsController < ApplicationController
 
 
 	def create
-		if current_client
-			@id = current_client.id
-		elsif current_user
-			@id = Client.find(params[:id]).id
-		end
-		@id
-			
-	  @homebuying = Homebuying.new(
-	  	{client_id: @id,
-	  		  	lender: params[:lender], 
-	  		  	hear_of_workshop: params[:hear_of_workshop],
-	  				contact_for_appointment: params[:contact_for_appointment],
-	  				real_estate_contract: params[:real_estate_contract],
-	  				realtor_name: params[:realtor_name],
-	  				realtor_phone: params[:realtor_phone],
-	  				property_address: params[:property_address],
-	  				property_state: params[:property_state],
-	  				property_city: params[:property_city],
-	  				loan_officer_name: params[:loan_officer_name],
-	  				loan_officer_email: params[:loan_officer_email],
-	  				loan_officer_phone: params[:loan_officer_phone],
-	  				payment_assistance_program: params[:payment_assistance_program],
-	  				approx_closing_date: params[:approx_closing_date]}
-	  	)
+    @id = current_client.id if current_client
+    @id = Client.find(params[:id]).id if current_user
+
+	  @homebuying = Homebuying.new({
+      client_id: @id,
+	  	lender: params[:lender], 
+	  	hear_of_workshop: params[:hear_of_workshop],
+			contact_for_appointment: params[:contact_for_appointment],
+			real_estate_contract: params[:real_estate_contract],
+			realtor_name: params[:realtor_name],
+			realtor_phone: params[:realtor_phone],
+			property_address: params[:property_address],
+			property_state: params[:property_state],
+			property_city: params[:property_city],
+			loan_officer_name: params[:loan_officer_name],
+			loan_officer_email: params[:loan_officer_email],
+			loan_officer_phone: params[:loan_officer_phone],
+			payment_assistance_program: params[:payment_assistance_program],
+			approx_closing_date: params[:approx_closing_date],
+    })
 	  
     
     if @homebuying.save
--- spec/controllers/homebuyings_controller_spec.rb
+++ spec/controllers/homebuyings_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
+require 'spec_helper'
+require 'rspec/rails'
+require 'devise'
 
 RSpec.describe HomebuyingsController, type: :controller do
+
+  include Devise::TestHelpers
 
 	describe "index #GET" do
 		it "assigns an array of Homebuyings" do
@@ -32,4 +37,61 @@ RSpec.describe HomebuyingsController, type: :controller do
         expect(response).to render_template :new
       end
     end
+
+    describe "create #GET" do
+      describe "properly assigns id based on users status" do
+        before :each do
+          @some_client = create(:client)
+          @some_other_client = create(:client)
+          @some_user = create(:user)
+        end
+        context "current_client logged in" do
+          before :each do
+            sign_in @some_client
+          end
+          it "assigns an id equal to the currently logged in clients id" do
+            post :create
+            expect(assigns(:id)).to eq @some_client.id
+          end
+        end
+
+        context "current_user logged in" do
+          before :each do
+            sign_in @some_user
+          end
+          it "assigns a client id based on the id param" do
+            post :create, id: @some_other_client.id
+            expect(assigns(:id)).to eq @some_other_client.id
+          end
+        end
+
+      end
+
+      context "neither user or client logged in" do
+        it "doesnt assign id if no user or client is logged in" do
+          post :create
+          expect(assigns(:id)).to eq nil
+        end
+      end
+
+      describe "creates a new homebuying instance and assigns it to homebuying" do
+        it "assigns a homebuying instance" do
+          get :create
+          expect(assigns(:homebuying)).to be_a_new(Homebuying)
+        end
+      end
+
+      describe "it attempts to save the homebuying instance" do
+        context "saves successfully" do
+          it "creates a new programemployee" do
+            sign_in create(:client)
+            expect{post :create, attributes_for(:homebuying)}.to change(ProgramEmployee, :count).by(1)
+          end
+        end
+
+        context "fails to save" do
+        end
+      end
+    end
+
 end
