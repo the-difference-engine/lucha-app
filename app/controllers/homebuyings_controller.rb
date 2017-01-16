0f77608cc9e6f04a9c021a80ca0774cc3cc69d35
jgates <jgates@leapfrogonline.com>
2017-01-16 16:50:22 -0600

    reordered tests and removed programEmployee

--- app/controllers/homebuyings_controller.rb
+++ app/controllers/homebuyings_controller.rb
@@ -33,7 +33,6 @@ class HomebuyingsController < ApplicationController
     })
 
     if @homebuying.save
-      ProgramEmployee.create({programable_id: @homebuying.id, programable_type: "Homebuying"})
       flash[:success] = ["You've completed the homebuying application"]
       redirect_to "/clients/#{@homebuying.client_id}/status"
     else
@@ -42,6 +41,9 @@ class HomebuyingsController < ApplicationController
     end
   end
 
+  def show
+    @homebuying = Homebuying.find(params[:id])
+  end
 
   def edit
     if current_client
@@ -52,42 +54,15 @@ class HomebuyingsController < ApplicationController
   end
 
   def update
-    if current_client
-      @homebuying = current_client.homebuying
-    elsif current_user
-      @homebuying = Homebuying.find(params[:id])
+    @homebuying = current_client.homebuying if current_client
+    @homebuying = Homebuying.find(params[:id]) if current_user
+
+    if @homebuying.update(homebuying_params)
+      flash[:success] = "homebuying application submitted."
+      redirect_to '/clients/#{@homebuying.client_id}'
+    else
+      render :edit
     end
-
-    @homebuying.update_attributes(homebuying_params)
-    respond_with @homebuying
-
-    # @client = current_client
-    # @homebuying = Homebuying.where(client_id: homebuying.client.id)
-    # if @homebuying.update(
-   #      lender: params[:lender],
-   #      hear_of_workshop: params[:hear_of_workshop],
-    #     contact_for_appointment: params[:contact_for_appointment],
-    #     real_estate_contract: params[:real_estate_contract],
-    #     realtor_name: params[:realtor_name],
-    #     realtor_phone: params[:realtor_phone],
-    #     property_address: params[:property_address],
-    #     property_state: params[:property_state],
-    #     property_city: params[:property_city],
-    #     loan_officer_name: params[:loan_officer_name],
-    #     loan_officer_email: params[:loan_officer_email],
-    #     loan_officer_phone: params[:loan_officer_phone],
-    #     payment_assistance_program: params[:payment_assistance_program],
-    #     approx_closing_date: params[:approx_closing_date]
-  #       )
-    #   flash[:success] = "homebuying application submitted."
-    #   redirect_to '/clients/#{@homebuying.client_id}'
-    # else
-    #   render :edit
-    # end
-  end
-
-  def show
-    @homebuying = Homebuying.find(params[:id])
   end
 
   def destroy
--- spec/controllers/homebuyings_controller_spec.rb
+++ spec/controllers/homebuyings_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe HomebuyingsController, type: :controller do
 
   include Devise::TestHelpers
 
+  # INDEX
   describe "index #GET" do
     it "assigns an array of Homebuyings" do
       homebuying1 = create(:homebuying)
@@ -24,6 +25,7 @@ RSpec.describe HomebuyingsController, type: :controller do
     end
   end
 
+  # NEW
   describe "new #GET" do
     it "assigns a homebuying instance" do
       get :new
@@ -36,7 +38,151 @@ RSpec.describe HomebuyingsController, type: :controller do
     end
   end
 
+  # CREATE
   describe "create #POST" do
+    describe "properly assigns id based on users status" do
+      before :each do
+        @some_client = create(:client)
+        @some_other_client = create(:client)
+        @some_user = create(:user)
+      end
+      context "current_client logged in" do
+        before :each do
+          sign_in @some_client
+        end
+        it "assigns an id equal to the currently logged in clients id" do
+          post :create
+          expect(assigns(:id)).to eq @some_client.id
+        end
+      end
+
+      context "current_user logged in" do
+        before :each do
+          sign_in @some_user
+        end
+        it "assigns a client id based on the id param" do
+          post :create, id: @some_other_client.id
+          expect(assigns(:id)).to eq @some_other_client.id
+        end
+      end
+    end
+
+    context "neither user or client logged in" do
+      it "doesnt assign id if no user or client is logged in" do
+        post :create
+        expect(assigns(:id)).to eq nil
+      end
+    end
+
+    describe "creates a new homebuying instance and assigns it to homebuying" do
+      it "assigns a homebuying instance" do
+        get :create
+        expect(assigns(:homebuying)).to be_a_new(Homebuying)
+      end
+    end
+
+    describe "it attempts to save the homebuying instance" do
+      context "saves successfully" do
+        before :each do
+          @this_client = create(:client)
+          sign_in @this_client
+        end
+        it "updates the flash hash with succuss message" do
+          post :create, attributes_for(:homebuying)
+          expect(flash[:success]).to be_present
+        end
+        it "redirects to the client status page" do
+          post :create, attributes_for(:homebuying)
+          expect(response).to redirect_to("/clients/#{@this_client.id}/status")
+        end
+      end
+
+      context "fails to save" do
+        before :each do
+          @this_client = create(:client)
+          sign_in @this_client
+          Homebuying.any_instance.stub(save: false)
+          Homebuying.any_instance.stub_chain(:errors, :full_messages).and_return(["danger"])
+        end
+        it "updates the flash message to danger" do
+          post :create, attributes_for(:homebuying)
+          expect(flash[:danger]).to be_present
+        end
+        it "renders the NEW template" do
+          post :create, attributes_for(:homebuying)
+          expect(response).to render_template :new
+        end
+      end
+    end
+  end
+
+  # SHOW
+  describe "show #GET" do
+    before :each do
+      @this_homebuying = create(:homebuying)
+    end
+    it "assigns a homebuying instance" do
+      get :show, id: @this_homebuying
+      expect(assigns(:homebuying)).to eq(@this_homebuying)
+    end
+
+    it "renders new template" do
+      get :show, id: @this_homebuying
+      expect(response).to render_template :show
+    end
+  end
+
+  # EDIT
+  describe "edit #GET" do
+    context "client signed in" do
+      it "assigns a homebuying to @homebuying" do
+        some_client = create(:client)
+        some_homebuying = create(:homebuying, client_id: some_client.id)
+        sign_in some_client
+        get :edit, id: some_homebuying
+        expect(assigns(:homebuying)).to eq(some_client.homebuying)
+      end
+    end
+    context "user signed in" do
+      it "assigns a homebuying to @foreclosure" do
+        some_user = create(:user)
+        some_homebuying = create(:homebuying)
+        sign_in some_user
+        get :edit, id: some_homebuying
+        expect(assigns(:foreclosure)).to eq(some_homebuying)
+      end
+    end
+    it "renders the #edit template" do
+      get :edit, id: create(:homebuying)
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "patch #update" do
+    context "current_client logged in" do
+      before :each do
+        sign_in @some_client
+        expect(assigns(:homebuying))
+      end
+      it "assigns an id equal to the currently logged in clients id" do
+        post :create
+        expect(assigns(:id)).to eq @some_client.id
+      end
+    end
+
+    context "current_user logged in" do
+      before :each do
+        sign_in @some_user
+      end
+      it "assigns a client id based on the id param" do
+        post :create, id: @some_other_client.id
+        expect(assigns(:id)).to eq @some_other_client.id
+      end
+    end
+  end
+
+  # UPDATE
+  describe "update #PATCH" do
     describe "properly assigns id based on users status" do
       before :each do
         @some_client = create(:client)
@@ -118,43 +264,4 @@ RSpec.describe HomebuyingsController, type: :controller do
     end
   end
 
-  describe "edit #GET" do
-    context "client signed in" do
-      it "assigns a homebuying to @homebuying" do
-        some_client = create(:client)
-        some_homebuying = create(:homebuying, client_id: some_client.id)
-        sign_in some_client
-        get :edit, id: some_homebuying
-        expect(assigns(:homebuying)).to eq(some_client.homebuying)
-      end
-    end
-    context "user signed in" do
-      it "assigns a homebuying to @foreclosure" do
-        some_user = create(:user)
-        some_homebuying = create(:homebuying)
-        sign_in some_user
-        get :edit, id: some_homebuying
-        expect(assigns(:foreclosure)).to eq(some_homebuying)
-      end
-    end
-    it "renders the #edit template" do
-      get :edit, id: create(:homebuying)
-      expect(response).to render_template :edit
-    end
-  end
-
-  describe "show #GET" do
-    before :each do
-      @this_homebuying = create(:homebuying)
-    end
-    it "assigns a homebuying instance" do
-      get :show, id: @this_homebuying
-      expect(assigns(:homebuying)).to eq(@this_homebuying)
-    end
-
-    it "renders new template" do
-      get :show, id: @this_homebuying
-      expect(response).to render_template :show
-    end
-  end
 end
