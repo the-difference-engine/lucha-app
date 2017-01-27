2171bcb31ccaf66ba4f8bc700a8be0f89d1b4614
verynear <scott.a.near@gmail.com>
2017-01-26 20:24:32 -0600

    successfully tested index, new, show, create user controller methods

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,9 +2,13 @@ FactoryGirl.define do
   factory :user do
     first_name "Jane"
     last_name "Doe"
-    sequence(:email){ |n| "janedoe#{n}@example.com"  }
+    sequence(:email){ |n| "janedoe#{n}@example.com" }
     password "12345678"
     password_confirmation "12345678"
+    home_phone "293-345-3457"
+    work_phone "432-234-6756"
+    cell_phone "876-456-3454"
   end
-
+  
 end
+
