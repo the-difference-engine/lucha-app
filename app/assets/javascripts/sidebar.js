2a3e76d0f98795f550bfc08c6ddaa7ec51d423eb
mrjonesbot <najones.arch@gmail.com>
2017-02-06 20:25:36 -0600

    added pills class, removed jquery

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
-  // $(".sidebar-nav a").on("click", function(){
-  //    $(".sidebar-nav").find(".active").removeClass("active");
-  //    $(this).parent().addClass("active");
-  // });
-  $(function(){
-    function stripTrailingSlash(str) {
-      if(str.substr(-1) == '/') {
-        return str.substr(0, str.length - 1);
-      }
-      return str;
-    }
-
-    var url = window.location.pathname;
-    var activePage = stripTrailingSlash(url);
-
-    $('.nav li a').each(function(){
-      var currentPage = stripTrailingSlash($(this).attr('href'));
-
-      if (activePage == currentPage) {
-        $(this).parent().addClass('active');
-      }
-    });
-  });
-});
+// $(document).ready(function(){
+//   // $(".sidebar-nav a").on("click", function(){
+//   //    $(".sidebar-nav").find(".active").removeClass("active");
+//   //    $(this).parent().addClass("active");
+//   // });
+//   $(function(){
+//     function stripTrailingSlash(str) {
+//       if(str.substr(-1) == '/') {
+//         return str.substr(0, str.length - 1);
+//       }
+//       return str;
+//     }
+//
+//     var url = window.location.pathname;
+//     var activePage = stripTrailingSlash(url);
+//
+//     $('.nav li a').each(function(){
+//       var currentPage = stripTrailingSlash($(this).attr('href'));
+//
+//       if (activePage == currentPage) {
+//         $(this).parent().addClass('active');
+//       }
+//     });
+//   });
+// });
