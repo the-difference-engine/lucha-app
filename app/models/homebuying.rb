15931948cd332c2786268551e7e0fe6f47c5f52e
bilal-ch <bilalchaudhary786@yahoo.com>
2017-02-20 19:04:44 -0600

    added csv model method to rental model

--- app/models/homebuying.rb
+++ app/models/homebuying.rb
@@ -20,11 +20,11 @@ class Homebuying < ActiveRecord::Base
 
 	belongs_to :client
   def self.to_csv(options = {})
-      CSV.generate(options) do |csv|
-        csv << column_names
-        all.each do |homebuying|
-          csv << homebuying.attributes.values_at(*column_names)
-        end
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |homebuying|
+        csv << homebuying.attributes.values_at(*column_names)
       end
     end
+  end
 end
