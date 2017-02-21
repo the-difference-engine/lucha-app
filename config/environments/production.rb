60003400e48a8ef0590647142d7c40cc920bf757
Sam S <SamS@tests-MacBook-Pro.local>
2017-02-20 20:01:46 -0600

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+   :user_name => ENV['SENDGRID_USER'],
+   :password => ENV['SENDGRID_PASSWORD'],
+   :domain => 'localhost:3000',
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :enable_starttls_auto => true
+ }
+config.action_mailer.default_url_options = { :host => 'qa-lucha-app.herokuapp.com' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
