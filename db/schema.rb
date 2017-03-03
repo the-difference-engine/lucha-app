abe5ee86f247cae652e24fd52e16f7dc9b5f7aa9
verynear <scott.a.near@gmail.com>
2017-03-02 20:44:32 -0600

    fixed migration error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303021203) do
+ActiveRecord::Schema.define(version: 20170303021041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
